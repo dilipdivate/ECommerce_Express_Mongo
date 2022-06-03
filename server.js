f424356542890729d4f432594facd54079273b22
dilipdivate <dilip.divate@gmail.com>
2022-06-03 23:59:26 +1000

    update host

--- server.js
+++ server.js
@@ -17,7 +17,7 @@ dotenv.config({ path: 'config/config.env' });
 
 // Connecting to database
 connectDatabase();
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 4000;
 const server = app.listen(PORT, () => {
   console.log(`Server is working on http://localhost:${PORT}`);
 });
